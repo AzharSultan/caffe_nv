904ebff46bb0aeaa997f92c922c93ff4fc25de2e
AzharSultan <azh.sultan@gmail.com>
2016-11-18 13:47:45 +0100

    Update domain_transform_layer.hpp

--- include/caffe/layers/domain_transform_layer.hpp
+++ include/caffe/layers/domain_transform_layer.hpp
@@ -67,12 +67,12 @@ class DomainTransformLayer : public Layer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   Dtype ComputeSigma(const int iter);
 
